02a5b69c65c8cb764ea81be6557d960328cf1c58
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-08-11 11:32:39 +0200

    fmt: rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
-//! Layers, extractors and template engine wrappers for 
+//! Layers, extractors and template engine wrappers for
 //! [axum](https://github.com/tokio-rs/axum) based Web MVC applications
-//! 
+//!
 //! # Getting started
-//! 
+//!
 //! ## `Cargo.toml`
-//! 
+//!
 //! ```toml
 //! [dependencies]
 //! axum-template = "0.1.0"
 //! ```
-//! 
-//! The [`engine`] module contains detailed usage examples for each of the 
+//!
+//! The [`engine`] module contains detailed usage examples for each of the
 //! supported template engines.
-//! 
+//!
 //! # Available features
-//! 
+//!
 //! - `handlebars`: Enables [`handlebars`](https://crates.io/crates/handlebars) support
 //! - `minijinja`: Enables [`minijinja`](https://crates.io/crates/minijinja) support
 //! - `tera`: Enables [`tera`](https://crates.io/crates/tera) support
-//! 
+//!
 //! # Useful links
-//! 
+//!
 //! - [Documentation](https://docs.rs/axum-template)
 //! - [Examples](https://github.com/Altair-Bueno/axum-template/examples)
 //! - [Source code](https://github.com/Altair-Bueno/axum-template)
@@ -59,7 +59,7 @@
     nonstandard_style,
     missing_debug_implementations,
     missing_docs,
-    rustdoc::missing_doc_code_examples,
+    rustdoc::missing_doc_code_examples
 )]
 #![deny(unreachable_pub, private_in_public)]
 #![forbid(unsafe_code)]
