8c1450741d3f359f7a1de5aa9213e561057630af
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-10-13 12:52:08 +0200

    tests: Added regression tests for #8

---
+++ tests/error.rs
@@ -0,0 +1,44 @@
+//! Regression tests for stack overflow on IntoResponse calls for error types
+//!
+//! See https://github.com/Altair-Bueno/axum-template/issues/8
+
+use axum::response::IntoResponse;
+use axum_template::engine::Engine;
+use axum_template::Render;
+use rstest::*;
+
+#[cfg(feature = "tera")]
+#[rstest]
+#[trace]
+#[tokio::test]
+async fn tera_error_into_response_check_infinite_recursion() -> anyhow::Result<()> {
+    let engine = tera::Tera::new("*.nothing")?;
+    let engine = Engine::new(engine);
+    let data = ();
+    _ = Render("", engine, data).into_response();
+    Ok(())
+}
+
+#[cfg(feature = "handlebars")]
+#[rstest]
+#[trace]
+#[tokio::test]
+async fn handlebars_error_into_response_check_infinite_recursion() -> anyhow::Result<()> {
+    let engine = handlebars::Handlebars::new();
+    let engine = Engine::new(engine);
+    let data = ();
+    _ = Render("", engine, data).into_response();
+    Ok(())
+}
+
+#[cfg(feature = "minijinja")]
+#[rstest]
+#[trace]
+#[tokio::test]
+async fn minijinja_error_into_response_check_infinite_recursion() -> anyhow::Result<()> {
+    let engine = minijinja::Environment::new();
+    let engine = Engine::new(engine);
+    let data = ();
+    _ = Render("", engine, data).into_response();
+    Ok(())
+}
